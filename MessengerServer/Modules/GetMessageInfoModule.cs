f97dc50cdcfb75ee542b4b64cca420ae01f57e6d
Andrey Kulagin <mr.and.kul@gmail.com>
2016-11-03 05:01:10 +0300

    Add GetMessageInfoModule

---
+++ MessengerServer/Modules/GetMessageInfoModule.cs
@@ -0,0 +1,50 @@
+﻿using MessengerServer.Models;
+using MessengerServer.Security;
+using Nancy;
+using Nancy.Security;
+
+namespace MessengerServer.Modules
+{
+    public class GetMessageInfoModule : NancyModule
+    {
+        public GetMessageInfoModule()
+        {
+            this.RequiresAuthentication();
+
+            Post["/getmessageinfo/{message_id:long}"] = parameters =>
+            {
+                long messageId = parameters["message_id"];
+
+                var userId = ((Identity) Context.CurrentUser).Id;
+
+                using (var db = new MessengerDbContext())
+                {
+                    var user = db.Users.Find(userId);
+
+                    var message = db.Messages.Find(messageId);
+
+                    if (message == null)
+                        return HttpStatusCode.BadRequest;
+
+                    var chat = message.Chat;
+
+                    if (!chat.Users.Contains(user))
+                        return HttpStatusCode.Forbidden;
+
+                    var fromUser = message.From;
+
+                    var result = new
+                    {
+                        MessageId = messageId,
+                        ChatId = message.ChatId,
+                        FromUserId = fromUser.Id,
+                        FromUserName = fromUser.GetFullNameOrLogin(),
+                        SentTime = message.SentTime.ToUniversalTime().ToBinary()
+                    };
+
+                    return result;
+                }
+            };
+        }
+    }
+}
